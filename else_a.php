b46276fe0dc222336dd65273cf66dda3d8761ff5
FernandoHerreroGarcia <ferhoz@hotmail.com>
2019-10-22 18:26:13 +0200

    php

--- else_a.php
+++ else_a.php
@@ -1,19 +1,8 @@
 <?php
-	function notas($n){
-			$notas=[
-			'1',
-			'2',
-			'3',
-			'4',
-			'5',
-			'6',
-			'7',
-			'8',
-			'9',
-			'10',
-			];
-	return $notas[$n-1];
-}	
-for($i=1;$i<=12;$i=$i+1)
-		if(!strstr(meses($i),'r'))
-			echo meses($i).'<br>';
+
+$nota = 3;
+$r = 'NO APTO';
+if($nota<5)
+	$r= 'NO APTO';
+	
+	echo $r;
