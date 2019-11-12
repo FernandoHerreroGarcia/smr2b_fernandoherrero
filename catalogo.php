37cbe7793cfcb772f22abee5047eda8c256033bf
Fernando Herrero García <55751672+FernandoHerreroGarcia@users.noreply.github.com>
2019-11-12 10:17:39 +0100

    Add files via upload

---
+++ catalogo.php
@@ -0,0 +1,15 @@
+#Enlaces a perfiles de Github del profesor y de los compañeros
+
+
+https://github.com/FernandoHerreroGarcia
+https://github.com/MarioPerezRodriguez
+https://github.com/MiguelCalderonPuertas
+https://github.com/
+https://github.com/
+https://github.com/
+https://github.com/
+https://github.com/
+https://github.com/
+https://github.com/
+https://github.com/
+https://github.com/
---
+++ css.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<title>Fernando Herrero</title>
+</head>
+
+<body style ="background-color:#66C7DF;
+	text-align:center;
+">
+
+<div style="margin:120px;padding;120px">
+<table><tr><td>
+<h1>Don Quijote de la Mancha<h1>
+
+<h2>En un lugar de la mancha de cuyo nombre no me quiero acordar</h2>
+
+<h3>2.2: ¿Cómo empezó todo?</h3>
+
+<h4 style="
+	color:green;
+	font-size:.10em;
+">
+</h4>
+
+<form>
+
+<input type="text">
+<input type="submit">
+
+</form>
+
+<p></p>
+
+<table style="
+	 border: 1px solid black;
+">
+  <tr><td>Galicia</td><td>La Coruña</td></tr>
+  <tr><td>Galicia</td><td>Lugo</td></tr>
+  <tr><td>Galicia</td><td>Orense</td></tr>
+  <tr><td>Galicia</td><td>Pontevedra</td></tr>
+  <tr><td>Asturias</td><td>Gijón</td></tr>
+  <tr><td>Asturias</td><td>Gijón</td></tr>
+  <tr><td>Cantabria</td><td>Cantabria</td></tr>
+  <tr><td>Pais Vasco</td><td>Álava</td></tr>
+  <tr><td>Pais Vasco</td><td>Vizcaya</td></tr>
+  <tr><td>Pais Vasco</td><td>Guipúzcoa</td></tr>
+  
+	
+	</table>
+<div style="
+	text-align:center;
+">
+
+<span style="
+	font-weight:bold;
+	text-decoration: underline;
+">En un lugar de La Mancha</span>, de cuyo nombre no quiero acordarme...
+<p>
+<img src="https://estaticos.muyhistoria.es/media/cache/400x300_thumb/uploads/images/pyr/58763d8b5cafe8e5bd8b45bc/quijoteysancho-c_0.jpg" width="400">
+
+
+<p><?php echo file_get_contents('http://loripsum.net/api');?></p>
+
+</div>
+</td></tr></table>
+</div>
+</body>
+</html>
