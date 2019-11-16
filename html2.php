f2da49c4b8046d657f1719269000f9a5e55f8dc0
Fernando Herrero García <55751672+FernandoHerreroGarcia@users.noreply.github.com>
2019-11-16 12:33:08 +0100

    Add files via upload

---
+++ html2.php
@@ -0,0 +1,68 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>DQdlM</title>
+	</head>
+	<body bgcolor="#66C7DF">
+		<center>
+			<h1>
+				<font size="10" face="verdana" color="blue">
+					<i>Don Quijote de la Mancha</i>
+				</font>
+			</h1>
+			<h2>
+				<p>
+					<u>En un lugar de la mancha de cuyo nombre no me quiero acordar</u>
+				</p>
+			</h2>
+			<img src="CastillaLaMancha.jpg" width="700" height="400">
+			</center>
+			<center>
+				<form>
+					<br/>
+					<b>Nombre:</b>
+					<input type="" name="" value="">
+						<br/>
+						<br/>
+						<b>Apellido:</b>
+						<input type="" name="" value="">
+							<br/>
+							<br/>
+							<input type="submit" name="Submit">
+							</form>
+						</center>
+						<br>
+							<center>
+								<table border="1">
+									<tr>
+										<th>Provincia</th>
+										<th>Ciudades</th>
+									</tr>
+									<tr>
+										<td>Galicia</td>
+										<td>La Coruña</td>
+									</tr>
+									<tr>
+										<td>Asturias</td>
+										<td>Oviedo</td>
+									</tr>
+									<tr>
+										<td>Asturias</td>
+										<td>Gijon</td>
+									</tr>
+									<tr>
+										<td>Cantabria</td>
+										<td>Santander</td>
+									</tr>
+									<tr>
+										<td>Cantabria</td>
+										<td>Torrelavega</td>
+									</tr>
+									<tr>
+										<td>Pais Vasco</td>
+										<td>Bilbao</td>
+									</tr>
+								</table>
+							</center>
+						</body>
+					</html>
