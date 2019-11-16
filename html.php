30ecbd78cc51289d7e771f30fecf44973991e69c
Fernando Herrero García <55751672+FernandoHerreroGarcia@users.noreply.github.com>
2019-11-16 12:02:59 +0100

    Add files via upload

--- html.php
+++ html.php
@@ -6,7 +6,7 @@
   <body bgcolor="#66C7DF">
     <h1><font size="10" color="blue"><center><b>Don Quijote de la Mancha</b></center></font></h1>
     <p><h2><u><center>En un lugar de la mancha de cuyo nombre no me quiero acordar</center></u></h2></p>
-    <center><img src="windmills.jpg" width="700" height="400"></center>
+    <center><img src="CastillaLaMancha.jpg" width="700" height="400"></center>
     
 <center>
   <form>
