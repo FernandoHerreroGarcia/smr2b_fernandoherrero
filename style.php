9902f39af5e6abe20b0ec49b31bbce35c95eddae
Fernando Herrero García <55751672+FernandoHerreroGarcia@users.noreply.github.com>
2019-11-20 12:46:21 +0100

    Add files via upload

---
+++ style.php
@@ -0,0 +1,98 @@
+<!DOCTYPE HTML> 
+
+<html>
+ 
+  <head>
+    <title>DQdlM</title>
+<style>
+
+  body {
+  background-color: lightblue;
+  } 
+
+  h1 { 
+  font-style: italic;
+  font-weight: bold;
+  font-size: 60px;
+  width: 700px;
+  margin: auto;
+  }
+  
+  h2 {
+  color: red;
+  text-decoration: underline;
+  width: 700px;
+  margin: auto;
+  }
+  
+  h3 {
+  width: 700px;
+  margin: auto; 
+  }
+
+  h4 { 
+  font-weight: bold;
+  }
+
+</style>
+  
+  </head>
+  
+    
+      <h1>Don Quijote de la Mancha</h1></br>
+      <h2>En un lugar de la mancha de cuyo nombre no me quiero acordar...</h2></br>
+      <h3><img src="CastillaLaMancha.jpg" alt="Molinos" width="700" height="400"></h3>
+    
+
+
+<h4>
+  <center>
+  <form>
+  <br/> 
+  Nombre:<input type="" name="" value=""><br/>
+  <br/>
+  Apellido:<input type="" name="" value=""><br/>
+  <br/> 
+  <input type="submit" name="Submit">
+  </form>
+</center>
+</h4> 
+
+<br>
+
+<center>
+<table border="1">
+  <tr>
+    <th>Provincia</th>
+    <th>Ciudades</th>
+  </tr>
+  <tr>
+    <td>Galicia</td>
+    <td>La Coruña</td>
+  </tr>
+  <tr>
+    <td>Asturias</td>
+    <td>Oviedo</td>
+  </tr>
+   <tr>
+    <td>Asturias</td>
+    <td>Gijon</td>
+  </tr> 
+  <tr>
+    <td>Cantabria</td>
+    <td>Santander</td>
+  </tr> 
+  <tr>
+    <td>Cantabria</td>
+    <td>Torrelavega</td>
+  </tr>
+    <tr>
+    <td>Pais Vasco</td>
+    <td>Bilbao</td>
+  </tr>
+</table>
+</center>
+  
+ </body>  
+
+</html>
